5e9636c3e9503b297ded1d1a64496c0e27077a3d
KalyaniVooda <kalyanivooda@gmail.com>
2026-08-12 14:19:18 +0530

    Initial commit

---
+++ 1.python operators.py
@@ -0,0 +1,57 @@
+'''python operators'''
+
+a=20
+b=10
+#1.Arithmetic operators
+print("a+b:",a+b) # a+b: 30
+print("a-b:",a-b) #a-b: 10
+print("a*b:",a*b) # a*b: 200
+print("a/b:",a/b)  #a/b: 2.0
+print("a//b:",a//b) #a//b: 2
+print("a**b:",a**b) # a**b: 10240000000000
+print("a%b:",a%b)   #a%b: 0
+
+#2.Comparision operators
+print("a<b:",a<b)#a<b: False
+print("a>b:",a>b)#a>b: True
+print("a>=b:",a>=b)#a>=b: True
+print("a<=b:",a<=b)#a<=b: False
+print("a==b:",a==b)#a==b: False
+print("a!=b:",a!=b)#a!=b: True
+
+#3.Assignment operators
+a += 100
+print("a+=100:",a)#a+=100: 120
+a -= 20
+print("a-=20:",a)#a-=20: 100
+a *= 10
+print("a*=10:",a)#a*=10: 1000
+a /= 6
+print("a/=6:",a)#a/=6: 166.66666666666666
+a //= 5
+print("a//=5:",a)#a//=5: 33.0
+a %= 2
+print("a%=2:",a)#a%=2: 1.0
+a **= 2
+print("a**=2:",a)#a**=2: 1.0
+
+#4.Logical operators
+x = 20
+y = 10
+print("x%2==0 and y%5 ==0 :",x%2==0 and y%5==0)#x%2==0 and y%5 ==0 : True
+print("x%10==0 or y%10 ==0 :",x%10==0 or y%10==0)#x%10==0 or y%10 ==0 : True
+print("not x%10==0:",not x%10==0)#not x%10==0: False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
