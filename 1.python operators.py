02620073dac7cd504c14d4e193ba9b7c7604b0a3
KalyaniVooda <kalyanivooda@gmail.com>
2026-08-17 19:43:26 +0530

    commit

--- 1.python operators.py
+++ 1.python operators.py
@@ -42,6 +42,42 @@ print("x%2==0 and y%5 ==0 :",x%2==0 and y%5==0)#x%2==0 and y%5 ==0 : True
 print("x%10==0 or y%10 ==0 :",x%10==0 or y%10==0)#x%10==0 or y%10 ==0 : True
 print("not x%10==0:",not x%10==0)#not x%10==0: False
 
+#5.Membership operator
+s = 'python programming' 
+print('x in s:','x' in s) #x in s: False
+l = [1,2,3,4]
+print('5 not in l:',5 not in l)#5 not in l: True
+t = (4,5,6,7)
+print('6 not in t:',6 not in t)#6 not in t: False
+se = {2,3,3,4,5}
+print('3 not in se:',3 not in se)#3 not in se: False
+d = {1:1,2:4,3:6,4:16}
+print('6 in d:',6 in d)#6 in d: False
+
+#6.idnetity operator
+a = [1,2,3,4]
+b = [1,2,3,4]
+print('a==b:',a==b)#a==b: True
+print('a is b:',a is b)#a is b: False
+print('a is not b:',a is not b)#a is not b: True
+c=a
+print('a==c:',a==c)#a==c: True
+print('a is c:',a is c)#a is c: True
+id(a)
+id(b)
+id(c)
+
+#7.bitwise operator
+print('8 & 3:',8&3)#8 & 3: 0
+print('8 | 3:',8|3)#8 | 3: 11
+print('8 ^ 3:',8^3)#8<<3: 64
+print('~3:',~3)#~3: -4
+print('8<<3:',8<<3)#8<<3: 64
+print('16>>2:',16>>2)#16>>2: 4
+
+
+
+
 
 
 
